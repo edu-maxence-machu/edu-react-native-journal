419d9b593c680a163b93fe2e8fe875c8823fa15d
Maxence Machu <maxence@handytown.fr>
2020-07-23 13:35:41 +0200

    LocalStorage et useEffect

--- pages/BankAccount.js
+++ pages/BankAccount.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {View, Text, StyleSheet, ScrollView, TouchableOpacity} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function BankAccount(){
 
@@ -16,6 +17,16 @@ export default function BankAccount(){
     /* Mount */
     useEffect(() => {
         console.log('Je suis crée');
+
+        /*
+            On va chercher la valeur au chargement = [];
+        */
+        async function updateBankWithLocal(){
+            const value = await AsyncStorage.getItem('bank');
+            setBank(parseInt(value))
+        }
+
+        updateBankWithLocal();
     }, [])
 
     useEffect(() => {
@@ -24,6 +35,17 @@ export default function BankAccount(){
             setHistory([
                 ...history, `Je suis à ${bank} €`
             ])
+        }
+
+        /*
+            On ajout au AsyncStorage quand la valeur bank est modifiée
+        */
+        if(bank !== null){
+            try {
+                AsyncStorage.setItem('bank', bank.toString());
+            } catch(e){
+                console.log(e);
+            }
         }
 
         updateHistory();
